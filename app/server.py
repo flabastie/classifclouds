fb562c81639f1d191c42604a35aef03e6375d552
Francois Labastie <flabastie@hotmail.com>
2019-04-17 20:53:26 +0200

    Update server.py

--- app/server.py
+++ app/server.py
@@ -11,7 +11,7 @@ from fastai.vision import *
 # export_file_url = 'https://www.dropbox.com/s/v6cuuvddq73d1e0/export.pkl?raw=1'
 export_file_url = 'https://drive.google.com/uc?export=download&id=1KZRMyV6YMv96o_KtYLH0tT5DpXqscBAE'
 #export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
-export_file_name = 'export.pkl'
+export_file_name = 'export_clouds.pkl'
 
 classes = ['Altocumulus', 'Altostratus', 'Cirrocumulus', 'Cirrostratus', 'Cirrus', 'Cumulonimbus', 'Cumulus', 'Nimbostratus', 'Stratocumulus', 'Stratus'],
 path = Path(__file__).parent
