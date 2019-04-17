a00e71d41283c39d74504d4f398eaefa813d45c2
flabastie <flabastie@hotmail.com>
2019-04-17 13:16:26 +0000

    initial commit

--- app/server.py
+++ app/server.py
@@ -9,11 +9,11 @@ from fastai import *
 from fastai.vision import *
 
 # export_file_url = 'https://www.dropbox.com/s/v6cuuvddq73d1e0/export.pkl?raw=1'
-export_file_url = 'https://drive.google.com/uc?export=download&id=1iIi1774ppoftM876n9clG6fbaCAT9sSP'
+export_file_url = 'https://drive.google.com/uc?export=download&id=1KZRMyV6YMv96o_KtYLH0tT5DpXqscBAE'
 #export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
 export_file_name = 'export.pkl'
 
-classes = ['Aivazovsky', 'Cortes', 'Renoir', 'VanGogh']
+classes = ['Altocumulus', 'Altostratus', 'Cirrocumulus', 'Cirrostratus', 'Cirrus', 'Cumulonimbus', 'Cumulus', 'Nimbostratus', 'Stratocumulus', 'Stratus'],
 path = Path(__file__).parent
 
 app = Starlette()
